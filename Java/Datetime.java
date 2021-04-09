3742cf7e48561350414b33b40575b13c0ac6003b
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-09 12:23:09 +0530

    Java Templates Added

---
+++ Java/Datetime.java
@@ -0,0 +1,117 @@
+// 1. Write a Java program to create a Date object using the Calendar class. 
+
+// 2. Write a Java program to get and display information (year, month, day, hour, minute) of a default calendar. 
+
+// 3. Write a Java program to get the maximum value of the year, month, week, date from the current date of a default calendar. 
+
+// 4. Write a Java program to get the minimum value of year, month, week, date from the current date of a default calendar. 
+
+// 5. Write a Java program to get the current time in New York. 
+
+// 6. Write a Java program to get current full date and time. 
+
+// 7. Write a Java program to get the last day of the current month. 
+
+// 8. Write a Java program to get the last date of the month. 
+
+// 9. Write a Java program to calculate the first and last day of each week. 
+
+// 10. Write a Java program to get the name of the first and last day of a month. 
+
+// 11. Write a Java program to get the number of days of a month. 
+
+// 12. Write a Java program to get localized day-in-week name. 
+
+// 13. Write a Java program to get a day of the week of a specific date. 
+
+// 14. Write a Java program to get the current local time. 
+
+// 15. Write a Java program to add some hours to the current time. 
+
+// 16. Write a Java program to get a date after 2 weeks. 
+
+// 17. Write a Java program to get a date before and after 1 year compares to the current date. 
+
+// 18. Write a Java program to check a year is a leap year or not. 
+
+// 19. Write a Java program to get year and months between two dates. 
+
+// 20. Write a Java program to get current timestamp. 
+
+// 21. Write a Java program to get the current time in all the available time zones. 
+
+// 22. Write a Java program to get the dates 10 days before and after today. 
+
+// 23. Write a Java program to get the months remaining in the year. 
+
+// 24. Write a Java program to display the dates in the following formats. Go to the editor
+
+// Sample format :
+
+// Default format of LocalDate=2016-09-16                                                                        
+// 16::Sep::2016                                                                                                 
+// Default format of LocalDateTime=2016-09-16T11:46:01.455                                                       
+// 16::Sep::2016 11::46::01                                                                                      
+// Default format of Instant=2016-09-16T06:16:01.456Z                                                            
+// Default format after parsing = 2014-04-27T21:39:48
+
+// Click me to see the solution
+
+// 25. Write a Java program to get the information of current/given year. Go to the editor
+
+// Sample format :
+
+// Sample Output :
+// Current Year: 2001                                                                                            
+// Is current year leap year? false                                                                              
+// Length of the year: 365 days
+
+// Click me to see the solution
+
+// 26. Write a Java program to get the information of current/given month. Go to the editor
+
+// Sample format :
+
+// Sample Output :
+// Integer value of the current month: 2                                                                         
+// Length of the month: 28                                                                                       
+// Maximum length of the month: 29                                                                               
+// First month of the Quarter: JANUARY
+
+// Click me to see the solution
+
+// 27. Write a Java program to get the information of a given time. 
+
+// 28. Write a Java program to display the date time information before some hours and minutes from current date time. 
+
+// 29. Write a Java program to convert a string to date. 
+
+// 30. Write a Java program to compute the difference between two dates (year, months, days). 
+
+// 31. Write a Java program to compute the difference between two dates (Hours, minutes, milli, seconds and nano). 
+
+// 32. Write a Java program to calculate your age. 
+
+// 33. Write a Java program to get the next and previous Friday. 
+
+// 34. Write a Java program to get today's date at midnight time. 
+
+// 35. Write a Java program to extract date, time from the date string. 
+
+// 36. Write a Java program to convert a unix timestamp to date in Java. 
+
+// 37. Write a Java program to find seconds since 1970. 
+
+// 38. Write a Java program to calculate the difference between two dates in days. 
+
+// 39. Write a java program to convert String to date and time and vice a versa. 
+
+// 40. Write a Java program to display current date without time and current time without date. 
+
+// 41. Write a Java program to display combine local date and time in a single object. 
+
+// 42. Write a Java program to define a period of time using date-based values (Period) and a duration of time using time-based values (Duration). 
+
+// 43. Write a Java program to display all the available time zones with UTC and GMT. 
+
+// 44. Write a Java program to define and extract zone offsets. 
