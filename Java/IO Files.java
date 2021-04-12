8ea0dc9e3f27371a15dbeed41e2d88c701d9b9f4
Anay Deshpande <anaydesh1234@gmail.com>
2021-04-12 13:47:50 +0530

    Update IO Files.java

--- Java/IO Files.java
+++ Java/IO Files.java
@@ -7,6 +7,7 @@ public class Exercise1 {
      public static void main(String a[])
      {
         File file = new File("/home/students/");
+        String[] fileList = file.list();
         for(String name:fileList){
             System.out.println(name);
         }
