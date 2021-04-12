8eada20ca490cbed9da5f83da02a213aeec4b9c7
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-12 21:56:40 +0530

    Javascript Exercises Templates Added
    All templates added, proceed with adding solutions.

---
+++ Javascript/Drawing.js
@@ -0,0 +1,28 @@
+// 1. Write a JavaScript program to draw the following rectangular shape.
+// Expected Output :
+// rectagular shape
+
+
+// 2. Write a JavaScript program to draw a circle.
+// Expected Output :
+// draw a circle
+
+
+// 3. Write a JavaScript program to draw two intersecting rectangles, one of which has alpha transparency.
+// Expected Output :
+// intersecting rectangles
+
+
+// 4. Write a JavaScript program to draw the following right-angled triangle.
+// Expected Output :
+// right angled triangle
+
+
+// 5. Write a JavaScript program to draw the following diagram [use moveto() function].
+// Expected Output :
+// draw fun
+
+
+// 6. Write a JavaScript program to draw the following diagram [diagonal, white to black circles].
+// Expected Output :
+// diagonal, white to black circles
