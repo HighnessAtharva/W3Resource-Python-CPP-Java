8270d4496d0f5877bdacb05f8965075c0879169b
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2023-03-08 21:32:52 +0530

    Update Python/Lambda.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- Python/Lambda.py
+++ Python/Lambda.py
@@ -763,7 +763,7 @@ print(sort_mixed_list(mixed_list))
 # Sort the said list of strings(numbers) numerically:
 # ['-500', '-12', '0', '4', '7', '12', '45', '100', '200']
 def sort_numeric_strings(nums_str):
- return sorted(nums_str, key=lambda el: int(el))
+ return sorted(nums_str, key=int)
 nums_str = ['4','12','45','7','0','100','200','-12','-500']
 print("Original list:")
 print(nums_str)
